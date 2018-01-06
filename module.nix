d7664078ea44f57c3658c751bc2f9fc3b2299f69
Silvan Mosberger <infinisil@icloud.com>
2018-01-06 18:23:33 +0100

    nix-repl somewhat working, but it's pretty bad all around

--- module.nix
+++ module.nix
@@ -1,13 +1,52 @@
-{ config, ... }:
+{ pkgs, config, lib, ... }:
 
 {
-  systemd.services.nixbot = {
-    description = "Nix bot";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${(import ./. {}).nixbot}/bin/nixbot ${builtins.readFile ./auth}";
-      Restart = "on-failure";
-      RestartSec = 1;
+  containers.nixbot = {
+    config = {
+      systemd.services.nixbot = {
+        description = "Nix bot";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${(import ./. {}).nixbot}/bin/nixbot ${builtins.readFile ./auth}";
+          User = "nixbot";
+          Restart = "on-failure";
+          RestartSec = 1;
+          Environment="NIX_PATH=nixpkgs=${lib.cleanSource pkgs.path}";
+        };
+      };
+
+      environment.systemPackages = with pkgs; [
+        nix-repl
+      ];
+
+      nix = {
+        extraOptions = ''
+          build-timeout = 1
+          timeout = 1
+          max-silent-time = 1
+          allow-import-from-derivation = false
+        '';
+        useSandbox = true;
+        package = pkgs.nixUnstable;
+        binaryCaches = [];
+      };
+
+      users.extraUsers.admin =
+        { isNormalUser = true;
+          description = "Admin account";
+          extraGroups = [ "wheel" ];
+          password = "foobar";
+        };
+
+      users.extraUsers.nixbot =
+        { isNormalUser = true;
+          description = "nixbot account";
+        };
+
+      security.sudo.wheelNeedsPassword = false;
     };
+
+
+    autoStart = true;
   };
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.Posix.Signals
 import           Control.Concurrent
 import           Control.Concurrent.STM
 import           Control.Exception          (SomeException, handle)
+import System.Exit (exitFailure)
 import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.State.Strict
 import           Data.Aeson
 import           Data.Aeson.Types           (fieldLabelModifier)
 import           Data.ByteString.Lazy       (fromStrict, toStrict)
@@ -14,6 +19,7 @@ import qualified Data.List                  as L
 import qualified Data.Map                   as M
 import           Data.Maybe
 import           Data.Text
+import Data.Time.Clock
 import           GHC.Generics
 import           GitHub.Data.Name
 import qualified GitHub.Endpoints.Repos     as R
@@ -22,7 +28,11 @@ import qualified Network.AMQP.Worker        as W
 import qualified Network.HTTP.Simple        as H
 import           System.Environment
 import           System.IO
+import           System.Process             (createProcess, proc, std_in, std_out, waitForProcess, std_err, StdStream(CreatePipe), env, getProcessExitCode, ProcessHandle, interruptProcessGroupOf)
+import           System.Process.Internals
 import           Text.Regex.TDFA
+
+
 
 data Input = Input
   { in_from   :: String
@@ -104,7 +114,6 @@ writeDone var = atomically $ putTMVar var ()
 
 server :: A.SASLMechanism -> IO ()
 server auth = do
-
     conn <- A.openConnection'' (opts auth)
     var <- newEmptyTMVarIO
     A.addConnectionClosedHandler conn True $ writeDone var
@@ -135,7 +144,8 @@ server auth = do
       A.confirmSelect chan False
       putStrLn "disabled nowait"
 
-      tag <- A.consumeMsgs chan myQueue A.Ack (onMessage chan)
+      stateVar <- atomically $ newTVar $ initialState chan
+      tag <- A.consumeMsgs chan myQueue A.Ack (onMessage chan stateVar)
       putStrLn $ "Started consumer with tag " ++ show tag
 
       putStrLn $ "terminating if enter is pressed..."
@@ -158,8 +168,8 @@ publishMessage chan msg = do
     putStrLn $ "Published Message" ++ maybe "" (\s -> ", got sequence number " ++ show s) intMb
     return intMb
 
-onMessage :: A.Channel -> (A.Message, A.Envelope) -> IO ()
-onMessage chan (m, e) = do
+onMessage :: A.Channel -> TVar ServerState -> (A.Message, A.Envelope) -> IO ()
+onMessage chan stateVar (m, e) = do
   case decode $ A.msgBody m :: Maybe Input of
     Nothing -> do
       putStrLn $ "Message body invalid: " ++ show (A.msgBody m)
@@ -167,14 +177,20 @@ onMessage chan (m, e) = do
     Just msg -> do
       putStrLn $ "Valid message " ++ show msg
       forkIO $ do
-        replyBodies <- reply msg
+        state <- readTVarIO stateVar
+        (replyBodies, newState) <- runStateT (reply msg) state
+        atomically $ writeTVar stateVar newState
         sequence_ $ flip fmap replyBodies (\b -> publishMessage chan (Output (in_from msg) b))
       A.ackEnv e
 
 
-data State = State
-           { commands :: M.Map String String }
-           deriving Show
+data ServerState = ServerState
+           { commands   :: M.Map String String
+           , chanStdin :: M.Map String (ProcessHandle, Handle)
+           , channel :: A.Channel
+           }
+
+initialState = ServerState M.empty M.empty
 
 parse :: String -> [(String, Int)]
 parse s = fmap (\xs -> (xs !! 1, read $ xs !! 2)) matches where
@@ -186,18 +202,18 @@ parseNixpkgs s = fmap (\xs -> xs !! 1) matches where
   matches :: [[String]]
   matches = s =~ ("nixpkgs/([^[:space:]]+)+" :: String)
 
-getNixpkgs :: String -> IO (Maybe String)
+getNixpkgs :: MonadIO m => String -> m (Maybe String)
 getNixpkgs s = do
-  putStrLn $ "Trying to get contents for " ++ s
-  contents <- R.contentsFor "NixOS" "nixpkgs" (pack s) (Just "heads/master")
+  lg $ "Trying to get contents for " ++ s
+  contents <- liftIO $ R.contentsFor "NixOS" "nixpkgs" (pack s) (Just "heads/master")
   case contents of
     Left error -> do
-      putStrLn $ show error
+      lg $ show error
       return Nothing
     Right contents -> do
       return $ Just $ "https://github.com/NixOS/nixpkgs/tree/master/" ++ s
 
-nixpkgs :: String -> IO [String]
+nixpkgs :: MonadIO m => String -> m [String]
 nixpkgs s = fmap catMaybes . mapM getNixpkgs $ parseNixpkgs s
 
 prToInfo :: (String, Int) -> IO (Maybe String)
@@ -210,7 +226,97 @@ prToInfo (p, n) = do
 
   return $ Just $ i_url body ++ " (by " ++ i_author body ++ ", " ++ i_state body ++ "): " ++ i_title body
 
+evalNixForChannel :: (MonadIO m, MonadState ServerState m) => String -> String -> m ()
+evalNixForChannel chan nix = do
+  state <- get
+  (process, stdin) <- liftIO $ case M.lookup chan $ chanStdin state of
+    Nothing -> do
+      putStrLn "No process running for this channel"
+      newHandle (channel state)
+    Just (process', stdin') -> do
+      exitCode <- getProcessExitCode process'
+      case exitCode of
+        Nothing -> do
+          putStrLn "reusing existing process"
+          return (process', stdin')
+        Just _ -> do
+          putStrLn "Process was running but now exited"
+          newHandle (channel state)
+    
+  put state { chanStdin = M.insert chan (process, stdin) (chanStdin state) }
+  lg $ "Putting line " ++ nix ++ " to nix process for channel " ++ chan
+  liftIO $ hPutStrLn stdin nix
+  liftIO $ hFlush stdin
+  return ()
+    where
+      newHandle :: A.Channel -> IO (ProcessHandle, Handle)
+      newHandle channel = do
+        nixPath <- getEnv "NIX_PATH"
+        putStrLn "Starting process"
+        (Just stdin, Just stdout, Just stderr, handle) <- createProcess (proc "/run/current-system/sw/bin/nix-repl" [])
+          { std_in = CreatePipe
+          , std_out = CreatePipe
+          , std_err = CreatePipe
+          , env = Just [ ("NIX_REMOTE", "daemon")
+                       , ("NIX_PATH", nixPath)
+                       ]
+          }
+        hSetBuffering stdout LineBuffering
+        hSetBuffering stderr LineBuffering
+        time <- fmap (addUTCTime (-1)) getCurrentTime
+        _ <- hGetLine stdout
+        hPutStrLn stdin ":a import <nixpkgs> {}"
+        hPutStrLn stdin ":a lib"
+        hPutStrLn stdin ":a builtins"
+        hFlush stdin
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        _ <- hGetLine stdout
+        forkIO $ outputToChannel handle chan stderr channel time
+        forkIO $ outputToChannel handle chan stdout channel time
+        return (handle, stdin)
 
-reply :: Input -> IO [String]
+lg :: MonadIO m => String -> m ()
+lg = liftIO . putStrLn 
+
+outputToChannel :: MonadIO m => ProcessHandle -> String -> Handle -> A.Channel -> UTCTime-> m ()
+outputToChannel process chan handle channel lastTime = do
+  lg "Receiving stdout"
+  line <- liftIO $ hGetLine handle
+  time <- liftIO getCurrentTime
+  lg $ "Got \"" ++ line ++ "\""
+  if line == "" || L.isPrefixOf "nix-repl>" line || time `diffUTCTime` lastTime < 1 || line == "error: interrupted by the user" then do
+    lg "Not doing anything"
+    outputToChannel process chan handle channel lastTime else do
+      let actualMessage = L.take 200 line
+      liftIO $ publishMessage channel $ Output { out_target = chan, out_body = actualMessage }
+      lg "Sent message, waiting for confirmation..."
+      liftIO $ A.waitForConfirms channel
+      lg "Published line"
+      lg "Sending interrupt"
+      liftIO $ interrupt process
+      lg "Sent interrupt"
+      outputToChannel process chan handle channel time
+
+interrupt :: ProcessHandle -> IO ()
+interrupt ph = do
+  let (ProcessHandle pmvar _) = ph
+  ph_ <- takeMVar pmvar
+  case ph_ of
+    OpenHandle pid -> do  -- pid is a POSIX pid
+      signalProcess sigINT pid
+      putMVar pmvar ph_
+
+reply :: (MonadIO m, MonadState ServerState m) => Input -> m [String]
 reply (Input { in_from = "#bottest", in_sender = user, in_body = "hello!" }) = return ["Hello, " ++ user]
-reply (Input { in_body = body}) = liftM2 (++) (nixpkgs body) (fmap catMaybes . mapM prToInfo $ parse body)
+--reply (Input { in_from = chan, in_body = ('>':' ':nix) }) = evalNixForChannel chan nix >> return []
+reply (Input { in_body = body}) = do
+  a <- nixpkgs body
+  b <- fmap catMaybes . mapM (fmap liftIO prToInfo) $ parse body
+  return $ a ++ b
