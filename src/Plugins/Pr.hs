e7043dfaa98a35fff31a213808cc7f9348fb8fb7
Silvan Mosberger <infinisil@icloud.com>
2018-07-04 23:58:53 +0200

    Finish PR rewrite, much clean code

--- src/Plugins/Pr.hs
+++ src/Plugins/Pr.hs
@@ -1,20 +1,22 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Plugins.Pr (prPlugin) where
 
 import           Plugins
 
-import           Control.Monad          (mzero)
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Data.Aeson             (FromJSON, Value (..), decode,
-                                         defaultOptions, encode,
-                                         genericParseJSON, genericToEncoding,
-                                         parseJSON, toEncoding, (.:))
-import           Data.Maybe             (catMaybes)
-import           GHC.Generics           (Generic)
-import qualified Network.HTTP.Simple    as H
+import           Control.Monad             (mzero)
+import           Control.Monad.IO.Class    (MonadIO, liftIO)
+import           Control.Monad.Reader
+import           Data.Aeson
+import           Data.Maybe
+import           GHC.Generics              (Generic)
 import           Text.Regex.TDFA
+
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
+import           Network.HTTP.Types.Status
 
 data Issue = Issue
   { i_title  :: String
@@ -35,16 +37,18 @@ instance FromJSON Issue where
 
   parseJSON _ = mzero
 
-prToInfo :: (String, Int) -> IO (Maybe String)
-prToInfo (p, n) = do
-  putStrLn $ "Sending request to github for nixOS/" ++ p ++ "#" ++ show n
-
-  req <- fmap (H.setRequestHeader "user-agent" ["haskell"]) $ H.parseRequest $ "https://api.github.com/repos/nixOS/" ++ p ++ "/issues/" ++ show n
-  body <- H.getResponseBody <$> H.httpJSONEither req
-  case body of
-    Right result -> return $ Just $ i_url result ++ " (by " ++ i_author result ++ ", " ++ i_state result ++ "): " ++ i_title result
-    Left _ -> return Nothing
-
+fetchInfo :: MonadIO m => Manager -> ParsedIssue -> m (Maybe Issue)
+fetchInfo manager (ParsedIssue _ owner repo number) = do
+  response <- liftIO $ httpLbs request manager
+  if statusIsSuccessful (responseStatus response)
+    then return $ decode (responseBody response)
+    else do
+      liftIO $ print $ responseBody response
+      return Nothing
+  where
+    request :: Request
+    request = (parseRequest_ $ "https://api.github.com/repos/" ++ owner ++ "/" ++ repo ++ "/issues/" ++ show number)
+      { requestHeaders = [("user-agent", "haskell")] }
 
 type Owner = String
 type Repo = String
@@ -52,8 +56,8 @@ data ParseType = Hash | Link deriving Show
 
 data ParsedIssue = ParsedIssue ParseType Owner Repo Int deriving Show
 
-parseIssues :: Repo -> (Repo -> Owner) -> String -> [ParsedIssue]
-parseIssues defRep defOwner = map extract . match prRegex
+parseIssues :: Settings -> String -> [ParsedIssue]
+parseIssues Settings { defOwner, defRepo } = map extract . match prRegex
   where
     prRegex :: RegexMaker a CompOption ExecOption String => a
     prRegex = makeRegex $ "(([^ ]+)/)?([^ ]+)?#([[:digit:]]+)"
@@ -63,20 +67,37 @@ parseIssues defRep defOwner = map extract . match prRegex
     extract [full, _, _, _, "", owner, repo, _, numberStr] =
       ParsedIssue Link owner repo (read numberStr)
     extract [full, _, owner, repo, numberStr, _, _, _, _]
-      | null repo = ParsedIssue Hash (defOwner defRep) defRep number
+      | null repo = ParsedIssue Hash (defOwner defRepo) defRepo number
       | null owner = ParsedIssue Hash (defOwner repo) repo number
       | otherwise = ParsedIssue Hash owner repo number
       where number = read numberStr
 
-parsePRs :: String -> String -> [(String, Int)]
-parsePRs def str = parsed
+data Settings = Settings
+  { defOwner :: Repo -> Owner
+  , defRepo  :: Repo
+  , prFilter :: ParsedIssue -> Bool
+  }
+
+displayIssue :: ParsedIssue -> Issue -> String
+displayIssue parsed Issue { i_title, i_author, i_state, i_url } =
+  let common = " (by " ++ i_author ++ ", " ++ i_state ++ "): " ++ i_title
+  in case parsed of
+    (ParsedIssue Hash _ _ _)      -> i_url ++ common
+    (ParsedIssue Link _ _ number) -> "#" ++ show number ++ common
+
+prReplies :: MonadIO m => Settings -> String -> m [String]
+prReplies settings@Settings { prFilter } input = do
+  manager <- liftIO $ newManager tlsManagerSettings
+  fmap (uncurry displayIssue) . catMaybes . fmap sequence . zip filtered <$> mapM (fetchInfo manager) filtered
   where
-    matches :: [[String]]
-    matches = str =~ ("([[:alpha:]]+)?#([[:digit:]]+)" :: String)
-    parsed :: [(String, Int)]
-    parsed = map (\(a, b) -> (if null a then def else a, b)) . map (\xs -> (xs !! 1, read $ xs !! 2)) $ matches
+    filtered = filter prFilter $ parseIssues settings input
 
 prPlugin :: MonadIO m => MyPlugin () m
 prPlugin = MyPlugin () trans "pr"
   where
-    trans (nick, msg) = liftIO $ fmap catMaybes . mapM prToInfo . filter (\(repo, n) -> repo /= "nixpkgs" || n >= 1000). parsePRs "nixpkgs" $ msg
+    trans (nick, msg) = prReplies settings msg
+    settings = Settings
+      { defOwner = const "NixOS"
+      , defRepo = "nixpkgs"
+      , prFilter = const True
+      }
