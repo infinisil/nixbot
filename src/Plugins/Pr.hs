6f2c0e4533d60c84d9c0154e2b63f3a991e917d8
Silvan Mosberger <infinisil@icloud.com>
2018-07-05 21:52:57 +0200

    Use GitHub API, improve code

--- src/Plugins/Pr.hs
+++ src/Plugins/Pr.hs
@@ -17,44 +17,15 @@ import           Data.Maybe
 import           GHC.Generics              (Generic)
 import           Text.Regex.TDFA
 
+import qualified Data.Text                 as Text
+
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 import           Network.HTTP.Types.Status
 
-data Pull = Pull
-  { p_merged :: Bool
-  } deriving (Show, Generic)
-
-data Issue = Issue
-  { i_pr     :: Maybe String
-  , i_title  :: String
-  , i_state  :: String
-  , i_url    :: String
-  , i_author :: String
-  } deriving (Show, Generic)
-
-instance FromJSON Issue where
-  parseJSON (Object v) = do
-    title <- v .: "title"
-    state <- v .: "state"
-    url <- v .: "html_url"
-    user <- v .: "user"
-    case user of
-      Object o -> do
-        pr <- if H.member "pull_request" v then do
-          pull_request <- v .: "pull_request"
-          case pull_request of
-            Object x -> Just <$> x .: "url"
-            _        -> return Nothing
-
-          else return Nothing
-        Issue pr title state url <$> o.: "login"
-      _        -> mzero
-
-  parseJSON _ = mzero
-
-instance FromJSON Pull where
-  parseJSON (Object v) = Pull <$> v .: "merged"
+import           GitHub                    hiding (Owner, Repo)
+import           GitHub.Data.Id
+import           GitHub.Data.Name
 
 data IssueInfo = IssueInfo
   { issue_state  :: String
@@ -63,41 +34,36 @@ data IssueInfo = IssueInfo
   , issue_title  :: String
   } deriving Show
 
-fetchInfo :: (MonadLogger m, MonadIO m) => Manager -> ParsedIssue -> m (Maybe IssueInfo)
-fetchInfo manager (ParsedIssue _ owner repo number) = do
-  $(logInfoSH)$ "Making request to " ++ show request
-  response <- liftIO $ httpLbs request manager
-  if statusIsSuccessful (responseStatus response)
-    then case decode (responseBody response) of
-      Nothing -> return Nothing
-      Just issue@Issue { i_state, i_url, i_author, i_title, i_pr } -> do
-        $(logDebugSH)$ issue
-        case i_pr of
-          Nothing -> return $ Just $ defaultIssue
-          Just url -> do
-            $(logDebugSH)$ "Making request to " ++ show url
-            pullResponse <- liftIO $ httpLbs ((parseRequest_ url) { requestHeaders = [("user-agent", "haskell")] }) manager
-            $(logDebugSH)$ "Got body: " ++ show (responseBody pullResponse)
-            case decode (responseBody pullResponse) :: Maybe Pull of
-              Just Pull { p_merged = True } -> return $ Just $ defaultIssue
-                { issue_state = "merged"
-                }
-              _ -> return $ Just $ defaultIssue
-        where
-          defaultIssue = IssueInfo
-            { issue_state = i_state
-            , issue_url = i_url
-            , issue_author = i_author
-            , issue_title = i_title
-            }
-
-    else do
-      $(logErrorSH)$ "Failed to get info, error: " ++ show (responseBody response)
+fetchInfo :: (MonadLogger m, MonadIO m) => Manager -> ParsedIssue -> m (Maybe String)
+fetchInfo manager (ParsedIssue parseType owner repo number) = do
+  result <- liftIO . executeRequest' $ issueR owner' repo' number'
+  case result of
+    Left error -> do
+      $(logWarnSH)$ "Got error from github request: " ++ show error
       return Nothing
+    Right Issue { issueHtmlUrl = Nothing } -> return Nothing
+    Right issue@Issue { issueHtmlUrl = Just url } -> Just <$> do
+      state <- case (issuePullRequest issue, issueState issue) of
+        (Just _, StateClosed) -> do
+          pullResult <- liftIO $ executeRequest' $ pullRequestR owner' repo' number'
+          return $ case pullResult of
+            Right PullRequest { pullRequestMerged = True } -> "merged"
+            _                                              -> showState
+        _ -> return showState
+      return $ prefix ++ " (by " ++ author ++ ", " ++ state ++ "): " ++ title
+      where
+        author = show . simpleUserLogin . issueUser $ issue
+        title = show . issueTitle $ issue
+        prefix = case parseType of
+          Hash -> show url
+          Link -> "#" ++ show number
+        showState = case issueState issue of
+          StateClosed -> "closed"
+          StateOpen   -> "open"
   where
-    request :: Request
-    request = (parseRequest_ $ "https://api.github.com/repos/" ++ owner ++ "/" ++ repo ++ "/issues/" ++ show number)
-      { requestHeaders = [("user-agent", "haskell")] }
+    owner' = N $ Text.pack owner
+    repo' = N $ Text.pack repo
+    number' = Id number
 
 type Owner = String
 type Repo = String
@@ -127,17 +93,11 @@ data Settings = Settings
   , prFilter :: ParsedIssue -> Bool
   }
 
-displayIssue :: ParsedIssue -> IssueInfo -> String
-displayIssue parsed IssueInfo { issue_title, issue_author, issue_state, issue_url } =
-  let common = " (by " ++ issue_author ++ ", " ++ issue_state ++ "): " ++ issue_title
-  in case parsed of
-    (ParsedIssue Hash _ _ _)      -> issue_url ++ common
-    (ParsedIssue Link _ _ number) -> "#" ++ show number ++ common
 
 prReplies :: (MonadLogger m, MonadIO m) => Settings -> String -> m [String]
 prReplies settings@Settings { prFilter } input = do
   manager <- liftIO $ newManager tlsManagerSettings
-  fmap (uncurry displayIssue) . catMaybes . fmap sequence . zip filtered <$> mapM (fetchInfo manager) filtered
+  catMaybes <$> mapM (fetchInfo manager) filtered
   where
     filtered = filter prFilter $ parseIssues settings input
 
