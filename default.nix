01f75d1af83a67c19c004dfc6621c5290a3f36f8
Silvan Mosberger <infinisil@icloud.com>
2018-07-12 16:22:44 +0200

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {}
+{ nixpkgs ? builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/dae9cf6106da19f79a39714f183ed253c62b32c5.tar.gz";
+    sha256 = "0r3c00m96ldb9z81ay7vj8gnpk4bf8gjcdiad7mgxvwxr9ndskjx";
+  }
+, pkgs ? import nixpkgs {}
 , lib ? pkgs.lib
 }:
 
