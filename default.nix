acad3348baa7c9422509aa9bea505452ff322b5b
Silvan Mosberger <infinisil@icloud.com>
2018-08-23 21:29:27 +0200

    Remove stack support, clean up nix file, enable cabal new-style

--- default.nix
+++ default.nix
@@ -3,33 +3,18 @@
     sha256 = "0r3c00m96ldb9z81ay7vj8gnpk4bf8gjcdiad7mgxvwxr9ndskjx";
   }
 , pkgs ? import nixpkgs {}
-, lib ? pkgs.lib
 }:
 
 let
 
-  hpkgs = (import ./stack2nix.nix { inherit pkgs; }).override {
-    overrides = self: super: {
-      hnix = pkgs.haskell.lib.overrideCabal super.hnix (oldAttrs: {
-        libraryHaskellDepends = oldAttrs.libraryHaskellDepends ++ [
-          self.compact
-        ];
-      });
-    };
+  src = fetchGit ./.;
+
+  nixbot = pkgs.haskellPackages.callCabal2nix "nixbot" src {};
+
+  # Once developPackage allows the use of a source filter, this can be used instead
+  nixbot' = pkgs.haskellPackages.developPackage {
+    root = ./.;
   };
 
-  src = builtins.filterSource (absName: type: let
-    name = lib.removePrefix "${toString ./.}/" absName;
-  in ! (lib.hasSuffix ".stack-work" name
-    || lib.hasSuffix ".git" name
-    || name == "state"
-    || name == "result"
-    || (lib.hasSuffix ".nix" name && ! lib.hasPrefix "hnix" name && ! (name == "options.nix")))
-  ) ./.;
-
-  nixbot = hpkgs.nixbot.overrideAttrs (oldAttrs: {
-    inherit src;
-  });
-
 in
-  nixbot
+  if pkgs.lib.inNixShell then nixbot.env else nixbot
