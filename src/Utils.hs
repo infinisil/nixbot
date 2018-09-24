34ed50e824c45cd3c1e403a0420b5869dbd229f4
Silvan Mosberger <infinisil@icloud.com>
2018-09-24 16:27:34 +0200

    Implement mostMatching

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,4 @@
-module Utils (prettySeconds) where
+module Utils (prettySeconds, mostMatching) where
 
 import           Data.List
 import           Data.Maybe
@@ -21,3 +21,16 @@ prettySeconds relevant seconds = intercalate ", " relevantStrings
     format _ 0   = Nothing
     format str 1 = Just $ "1 " ++ str
     format str n = Just $ show n ++ " " ++ str ++ "s"
+
+
+-- | Returns the mapping of the largest list split for which the predicate still holds. The split will always be after the first element.
+mostMatching :: [a] -> (([a], [a]) -> b) -> (b -> Bool) -> Maybe b
+mostMatching xs f pred = beforeFirstFalse pred $ map (f . (`splitAt` xs)) [1..length xs]
+
+
+-- | Returns the first element for which the predicate returns True while returning False for the next element
+beforeFirstFalse :: (a -> Bool) -> [a] -> Maybe a
+beforeFirstFalse _ [] = Nothing
+beforeFirstFalse pred (x:xs)
+  | pred x = Just $ fromMaybe x (beforeFirstFalse pred xs)
+  | otherwise = Nothing
