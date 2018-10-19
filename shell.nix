33654ae1559ccb144a0c34d8db6a538efb088c9a
Silvan Mosberger <infinisil@icloud.com>
2018-10-20 00:11:14 +0200

    Update nixpksg and clean up nix build files

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 with import ./. {};
 
-shellFor {
-  packages = p: with p; [ nixbot nix-session ];
-  withHoogle = true;
-  nativeBuildInputs = [ cabal-install ];
+hpkgs.shellFor {
+  packages = p: with hpkgs; [ nixbot nix-session ];
+  nativeBuildInputs = [ hpkgs.cabal-install ];
 }
