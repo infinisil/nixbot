1e7be986b67d5964e4779404ef18fb47ffb4d952
Silvan Mosberger <infinisil@icloud.com>
2018-10-27 13:23:07 +0200

    Suppress config eval error from library

--- nix-session/src/Nix/Session/Config.hs
+++ nix-session/src/Nix/Session/Config.hs
@@ -3,16 +3,18 @@
 
 module Nix.Session.Config where
 
-import           Control.Lens         (makeLenses)
-import           Data.Aeson           (FromJSON (..), defaultOptions,
-                                       eitherDecode', fieldLabelModifier,
-                                       genericParseJSON)
-import           Data.Map             (Map)
+import           Control.Lens               (makeLenses)
+import           Data.Aeson                 (FromJSON (..), defaultOptions,
+                                             eitherDecode', fieldLabelModifier,
+                                             genericParseJSON)
+import qualified Data.ByteString.Lazy.Char8 as BS
+import           Data.Map                   (Map)
 import           Data.SafeCopy
-import           Data.Text            (Text)
-import           GHC.Generics         (Generic)
-import           Paths_nix_session    (getDataFileName)
-import           System.Process.Typed (proc, readProcessStdout_)
+import           Data.Text                  (Text, pack)
+import           GHC.Generics               (Generic)
+import           Paths_nix_session          (getDataFileName)
+import           System.Exit
+import           System.Process.Typed       (proc, readProcess)
 
 {-
 Plan for config:
@@ -68,7 +70,9 @@ evalConfig nixInstantiate config = do
   nixFile <- getDataFileName "nix/default.nix"
   let args = [ "--eval", "--strict", "--json"
              , "--arg", "config", config, nixFile ]
-  result <- readProcessStdout_ $ proc nixInstantiate args
-  case eitherDecode' result of
-    Left err -> fail $ "Couldn't decode json value, Nix module is inconsistent with internal representation: " ++ err
-    Right res -> return res
+  (code, stdout, stderr) <- readProcess $ proc nixInstantiate args
+  case code of
+    ExitSuccess -> case eitherDecode' stdout of
+      Left err -> fail $ "Couldn't decode json value, Nix module is inconsistent with internal representation: " ++ err
+      Right res -> return res
+    ExitFailure _ -> fail $ BS.unpack stderr
