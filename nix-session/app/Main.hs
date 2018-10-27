21eb0c5f742648d35927a3e3a43ac8873af92236
Silvan Mosberger <infinisil@icloud.com>
2018-10-27 17:48:43 +0200

    nix-session: Use haskeline for console IO

--- nix-session/app/Main.hs
+++ nix-session/app/Main.hs
@@ -2,39 +2,38 @@
 
 module Main where
 
-import           Control.Monad.Reader
-import           Control.Monad.State    (MonadState, evalStateT)
-import           Data.Maybe             (fromMaybe)
-import           Data.Text              (pack)
+import           Control.Monad.State      (StateT (..), evalStateT, runStateT)
+import           Control.Monad.Trans      (lift)
+import           Data.Maybe               (fromMaybe)
+import           Data.Text                (pack)
 import           Nix.Session
 import           Nix.Session.Config
+import           System.Console.Haskeline
 import           System.Directory
 import           System.FilePath
-import           System.IO
 
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-
+instance MonadException m => MonadException (StateT s m) where
+    controlIO f = StateT $ \s -> controlIO $ \(RunIO run) -> let
+                    run' = RunIO (fmap (StateT . const) . run . flip runStateT s)
+                    in fmap (flip runStateT s) $ f run'
 main :: IO ()
 main = do
   nixInstantiate <- fromMaybe (error "Couldn't find nix-instantiate executable") <$> findExecutable "nix-instantiate"
   config <- getConfig nixInstantiate
   menv <- initEnv config
-  putStr "> "
   case menv of
     Left err  -> fail err
-    Right env -> evalStateT thething env
+    Right env -> evalStateT (runInputT defaultSettings (withInterrupt thething)) env
 
-
-thething :: (MonadIO m, MonadState Env m) => m ()
-thething = do
-  eof <- liftIO isEOF
-  if eof then saveEnv
-  else do
-    line <- pack <$> liftIO getLine
-    result <- processText line
-    liftIO $ putStr result
-    liftIO $ putStr "> "
-    thething
+thething :: InputT (StateT Env IO) ()
+thething = handleInterrupt thething $ do
+  minput <- getInputLine "> "
+  case minput of
+    Nothing -> lift saveEnv
+    Just input -> do
+      result <- lift $ processText (pack input)
+      outputStr result
+      thething
 
 getConfig :: FilePath -> IO GlobalConfig
 getConfig nixInstantiate = do
