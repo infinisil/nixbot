7b337621310f8730bf2464c0f5cc6834ca666332
Silvan Mosberger <infinisil@icloud.com>
2018-11-24 22:00:48 +0100

    Update nixpkgs and hnix, ghc 8.4.4

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/0a7e258012b60cbe530a756f09a4f2516786d370.tar.gz";
-    sha256 = "1qcnxkqkw7bffyc17mqifcwjfqwbvn0vs0xgxnjvh9w0ssl2s036";
+    url = "https://github.com/NixOS/nixpkgs/archive/80738ed9dc0ce48d7796baed5364eef8072c794d.tar.gz";
+    sha256 = "0anmvr6b47gbbyl9v2fn86mfkcwgpbd5lf0yf3drgm8pbv57c1dc";
   }) {}
 }:
 let
@@ -28,12 +28,15 @@ let
       "^LICENSE$"
     ]) {};
 
-    hnix = pkgs.haskell.lib.overrideSrc super.hnix {
-      # https://github.com/haskell-nix/hnix/pull/363
-      src = fetchTarball {
-        url = "https://github.com/infinisil/hnix/archive/c723e73a415fa68d12baac88140d685424adac4f.tar.gz";
-        sha256 = "15fnylxm29izxvf7mf7f4y60spi2hgs72lkf53an8wk7is5v434f";
-      };
+    hnix = import (pkgs.fetchgit {
+      # https://github.com/haskell-nix/hnix/pull/416
+      url = "https://github.com/infinisil/hnix";
+      rev = "8ef09e6efa9ec7b5ec13519cac7241f0b3323df3";
+      sha256 = "0wz5z4p5lvqjhsxamn3xk9daidga68g71vgkwd3w2pvajprvd4xf";
+    }) {
+      inherit pkgs;
+      # Needed for some reason, not sure why
+      returnShellEnv = false;
     };
   });
 in hpkgs.nixbot // {
