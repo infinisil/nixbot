909908725ead73d12713a5bf7aa0192b4214f386
Silvan Mosberger <infinisil@icloud.com>
2018-11-27 22:41:00 +0100

    Better karma regex, support multiple in one message

--- src/Plugins/Karma.hs
+++ src/Plugins/Karma.hs
@@ -4,28 +4,30 @@ module Plugins.Karma (karmaPlugin) where
 import           Plugins
 
 import           Control.Monad.State
+import           Data.List           (intercalate)
 import           Data.Map            (Map)
 import qualified Data.Map            as M
 import           Data.Maybe          (listToMaybe)
 import           Text.Regex.TDFA     ((=~))
 
 karmaRegex :: String
-karmaRegex = "\\`[[:space:]]*([^[:space:]]+)[[:space:]]*\\+\\+\\'"
+karmaRegex = "([^[:space:]]+)\\+\\+"
+
+mods :: String -> [String] -> State (M.Map String Int) [(String, Bool)]
+mods = undefined
 
 karmaPlugin :: Monad m => MyPlugin (M.Map String Int) m
 karmaPlugin = MyPlugin M.empty trans "karma"
   where
-    trans (chan, nick, msg) =
-      case matches of
-        Nothing   -> return []
-        Just user -> do
-          let decrease = user == nick
-          let mod = if decrease then (\x -> x - 1) else (+1)
-          oldMap <- get
-          let newKarma = mod . M.findWithDefault 0 user $ oldMap
-          modify $ M.insert user newKarma
-          return [ user ++ "'s karma got " ++
-            (if decrease then "decreased" else "increased")
-            ++ " to " ++ show newKarma ]
-      where
-        matches = listToMaybe $ fmap (!!1) (msg =~ karmaRegex :: [[String]])
+    trans (chan, nick, msg) = do
+      let matches = fmap (!!1) (msg =~ karmaRegex :: [[String]])
+      messages <- forM matches $ \user -> do
+        let decrease = user == nick
+        let mod = if decrease then (\x -> x - 1) else (+1)
+        oldMap <- get
+        let newKarma = mod . M.findWithDefault 0 user $ oldMap
+        modify $ M.insert user newKarma
+        return $ user ++ "'s karma got " ++
+          (if decrease then "decreased" else "increased")
+          ++ " to " ++ show newKarma
+      return [ intercalate ", " messages | not $ null messages ]
