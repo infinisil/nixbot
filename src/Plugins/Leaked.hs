166a79cc112cbbcb2ff63214c5c04607bcb5f7e1
Silvan Mosberger <infinisil@icloud.com>
2018-12-22 02:50:18 +0100

    Fix leaked plugin for PM's

--- src/Plugins/Leaked.hs
+++ src/Plugins/Leaked.hs
@@ -14,12 +14,13 @@ regex = makeRegexOpts comp exec "nickserv identify .*" where
 leakedPlugin :: Plugin
 leakedPlugin = Plugin
   { pluginName = "leaked"
-  , pluginCatcher = \input -> if regex `matchTest` inputMessage input then Consumed input else PassedOn
-  , pluginHandler = \input@Input { inputUser, inputChannel = Just channel } ->
-      privMsg inputUser $ "You accidentally leaked your password in #" ++ channel ++ "! "
+  , pluginCatcher = \input -> case (regex `matchTest` inputMessage input, inputChannel input) of
+      (True, Just chan) -> Consumed (inputUser input, chan)
+      _                 -> PassedOn
+  , pluginHandler = \(user, chan) ->
+      privMsg user $ "You accidentally leaked your password in #" ++ chan ++ "! "
         ++ "Make sure to change it with `/msg nickserv set password <a-new-password>` as soon as "
         ++ "possible if you don't want anybody to take over your account. `/msg nickserv help "
         ++ "set password` for more info."
-
   }
   where
