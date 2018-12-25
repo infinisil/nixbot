c7839a5f6608e2ff5ed3baeca846469e62fb9775
Silvan Mosberger <infinisil@icloud.com>
2018-12-25 02:27:21 +0100

    Plugin system: Add getUser/getChannel functions
    Can be used to implement `reply` function

--- src/Plugins.hs
+++ src/Plugins.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes       #-}
@@ -31,6 +32,8 @@ class Monad m => PluginMonad m where
   getChannelState :: Channel -> m FilePath
   getUserState :: User -> m FilePath
   getChannelUserState :: Channel -> User -> m FilePath
+  getUser :: m User
+  getChannel :: m (Maybe Channel)
 
 data HandlerResult p = Consumed p
                      | PassedOn
@@ -43,22 +46,22 @@ data Plugin = forall p . Plugin
   , pluginHandler :: forall m . (MonadReader Config m, MonadLogger m, IRCMonad m, MonadIO m, PluginMonad m) => p -> m ()
   }
 
-newtype PluginT m a = PluginT { unPluginT :: FilePath -> String -> m a } deriving (Functor)
+newtype PluginT m a = PluginT { unPluginT :: (FilePath, String, Input) -> m a } deriving (Functor)
 
 instance Applicative m => Applicative (PluginT m) where
-  pure a = PluginT $ \_ _ -> pure a
-  PluginT a <*> PluginT b = PluginT $ \base pluginName -> let af = a base pluginName; bf = b base pluginName in af <*> bf
+  pure a = PluginT $ \_ -> pure a
+  PluginT a <*> PluginT b = PluginT $ \args -> let af = a args; bf = b args in af <*> bf
 
 instance Monad m => Monad (PluginT m) where
-  PluginT a >>= f = PluginT $ \base pluginName -> let
-    af = a base pluginName
-    in af >>= (\y -> unPluginT (f y) base pluginName)
+  PluginT a >>= f = PluginT $ \args -> let
+    af = a args
+    in af >>= (\y -> unPluginT (f y) args)
 
 instance MonadIO m => MonadIO (PluginT m) where
-  liftIO action = PluginT $ \_ _ -> liftIO action
+  liftIO action = PluginT $ \_ -> liftIO action
 
 instance MonadTrans PluginT where
-  lift a = PluginT $ \_ _ -> a
+  lift a = PluginT $ const a
 
 instance MonadLogger m => MonadLogger (PluginT m) where
 
@@ -73,28 +76,32 @@ instance IRCMonad m => IRCMonad (ReaderT r m) where
   isKnown user = lift $ isKnown user
 
 instance MonadIO m => PluginMonad (PluginT m) where
-  getGlobalState = PluginT $ \base pluginName -> do
+  getGlobalState = PluginT $ \(base, pluginName, _) -> do
     let dir = base </> "global" </> pluginName
     liftIO $ createDirectoryIfMissing True dir
     return dir
-  getChannelState channel = PluginT $ \base pluginName -> do
+  getChannelState channel = PluginT $ \(base, pluginName, _) -> do
     let dir = base </> "channel" </> channel </> pluginName
     liftIO $ createDirectoryIfMissing True dir
     return dir
-  getUserState user = PluginT $ \base pluginName -> do
+  getUserState user = PluginT $ \(base, pluginName, _) -> do
     let dir = base </> "user" </> user </> pluginName
     liftIO $ createDirectoryIfMissing True dir
     return dir
-  getChannelUserState channel user = PluginT $ \base pluginName -> do
+  getChannelUserState channel user = PluginT $ \(base, pluginName, _) -> do
     let dir = base </> "channel-user" </> channel </> user </> pluginName
     liftIO $ createDirectoryIfMissing True dir
     return dir
+  getUser = PluginT $ \(_, _, Input { inputUser }) -> return inputUser
+  getChannel = PluginT $ \(_, _, Input { inputChannel }) -> return inputChannel
 
 instance PluginMonad m => PluginMonad (ReaderT r m) where
   getGlobalState = lift getGlobalState
   getChannelState channel = lift $ getChannelState channel
   getUserState user = lift $ getUserState user
   getChannelUserState channel user = lift $ getChannelUserState channel user
+  getUser = lift getUser
+  getChannel = lift getChannel
 
 runPlugins :: (MonadLogger m, MonadReader Env m, MonadIO m, IRCMonad m) => [Plugin] -> Input -> m Bool
 runPlugins [] _ = return False
@@ -102,9 +109,15 @@ runPlugins (Plugin { pluginName, pluginCatcher, pluginHandler }:ps) input = case
   PassedOn -> runPlugins ps input
   Consumed p -> do
     cfg <- asks config
-    unPluginT (runReaderT (pluginHandler p) cfg) (stateDir cfg </> "new") pluginName
+    unPluginT (runReaderT (pluginHandler p) cfg) (stateDir cfg </> "new", pluginName, input)
     return True
 
+reply :: (IRCMonad m, PluginMonad m) => Message -> m ()
+reply msg = getChannel >>= \case
+  Nothing -> do
+    user <- getUser
+    privMsg user msg
+  Just chan -> chanMsg chan msg
 
 type PluginInput = (String, String, String)
 data MyPlugin s m = MyPlugin { initState :: s
