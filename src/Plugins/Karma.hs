f6c71006e6646a490e504b491946543b55abe78f
Silvan Mosberger <contact@infinisil.com>
2020-04-30 03:47:03 +0200

    Add some spice to karma messages
    Unrefined code though

--- src/Plugins/Karma.hs
+++ src/Plugins/Karma.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE LambdaCase          #-}
@@ -8,6 +9,7 @@ module Plugins.Karma (karmaPlugin) where
 
 import           Frontend.Types
 import           Plugins
+import           Log
 
 import           Config
 import           Control.Monad.Reader
@@ -16,6 +18,7 @@ import           Data.Maybe
 import qualified Data.Set             as Set
 import           Data.Text            (Text)
 import qualified Data.Text            as Text
+import Control.Concurrent (threadDelay)
 import           Data.Time
 import           GHC.Generics
 import           IRC
@@ -23,6 +26,9 @@ import           System.Directory
 import           System.FilePath
 import           Text.Regex.TDFA      ((=~))
 import           Types
+import Numeric
+import Data.Char (intToDigit)
+import System.Random
 
 
 karmaRegex :: String
@@ -82,6 +88,46 @@ matchFilter matches = do
       filtered = matchSet `Set.difference` blacklist
   return $ Set.toList filtered
 
+type KarmaAction = (Int -> Bool, Int -> Channel -> User -> PluginT App () -> PluginT App ())
+
+increaseMessages :: [(Double, KarmaAction)]
+increaseMessages =
+  [ (100, (const True, \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to " <> show' (n + 1))))
+  , (3, (const True, \n chan nick increase -> increase *> increase *> chanMsg chan (nick <> "'s karma got increased to " <> show' (n + 2) <> ", it's a crit!")))
+  , (2, (const True, \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to like " <> show' (n + 1) <> ", I think")))
+  , (2, (const True, \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to " <> show' (n + 1) <> ", that's Numberwang!")))
+  , (1, (const True, \_ chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased!")))
+  , (1, (const True, \n chan nick increase -> do
+            increase
+            chanMsg chan (nick <> "'s karma got decreased to " <> show' (n - 1))
+            liftIO $ threadDelay 10000000
+            chanMsg chan ("Wait no, it got *increased* to " <> show' (n + 1))
+    ))
+  , (1, (const True, \_ chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to -2147483648")))
+  , (3, (const True, \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to " <> show' (n + 1) <> ".00000000000000004")))
+  , (100, (\n -> length (show (n + 1)) > length (show n), \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma now has " <> show' (length (show (n + 1))) <> " digits!")))
+  , (2, ((>= 0), \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to 0b" <> Text.pack (showIntAtBase 2 intToDigit (n + 1) ""))))
+  , (2, ((>= 0), \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to 0o" <> Text.pack (showOct (n + 1) ""))))
+  , (2, ((>= 0), \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to 0x" <> Text.pack (showHex (n + 1) ""))))
+  , (1, ((>= 0), \_ chan nick increase -> increase *> chanMsg chan (nick <> " was put on Santa's \"nice\" list")))
+  ]
+
+selectWithFrequency :: Double -> [(Double, a)] -> a
+selectWithFrequency randomValue items = selectedItem
+  where
+    total = sum (map fst items)
+    selectedValue = total * randomValue
+    selectedIndex = length (filter (<=selectedValue) $ scanl (+) 0 $ map fst items) - 1
+    selectedItem = snd $ items !! selectedIndex
+    
+
+triggerIncrease :: Int -> Channel -> User -> PluginT App () -> PluginT App ()
+triggerIncrease oldAmount chan nick increase = do
+  randomValue :: Double <- liftIO randomIO
+  let potentialMessages = filter (\(_, (f, _)) -> f oldAmount) increaseMessages
+      selected = snd $ selectWithFrequency randomValue potentialMessages
+  selected oldAmount chan nick increase
+
 karmaPlugin :: Plugin
 karmaPlugin = Plugin
   { pluginName = "karma"
@@ -97,24 +143,10 @@ karmaPlugin = Plugin
         Right (channel, user) -> rateLimited channel user >>= \limited -> unless limited $ do
           time <- liftIO getCurrentTime
 
-          results <- forM matches $ \receiver ->
+          forM_ matches $ \receiver ->
             lift (isKnown receiver) >>= \case
-              False -> return Nothing
+              False -> return ()
               True -> do
-                -- TODO: Only allow karma for users that have talked before
-                let selfKarma = receiver == user
-                let entry = if selfKarma then SelfKarma
-                      { givenIn = channel
-                      , givenAt = time
-                      , karmaContext = inputMessage
-                      }
-                    else KarmaEntry
-                      { givenBy = user
-                      , givenIn = channel
-                      , givenAt = time
-                      , karmaContext = inputMessage
-                      }
-
                 receiverFile <- (</> "entries") <$> getUserState receiver
                 exists <- liftIO $ doesFileExist receiverFile
                 entries <- if not exists then return [] else
@@ -123,11 +155,25 @@ karmaPlugin = Plugin
                       liftIO $ putStrLn $ "Couldn't decode karma file: " ++ err
                       return []
                     Right entries -> return entries
-                let newEntries = entry : entries
-                liftIO $ encodeFile receiverFile newEntries
-                return $ Just $ receiver <> "'s karma got "
-                  <> (if selfKarma then "decreased" else "increased")
-                  <> " to " <> Text.pack (show (countKarma newEntries))
-
-          chanMsg channel $ Text.intercalate ", " $ catMaybes results
+                if receiver == user then do
+                  let entry = SelfKarma
+                        { givenIn = channel
+                        , givenAt = time
+                        , karmaContext = inputMessage
+                        }
+                  let newEntries = entry : entries
+                  liftIO $ encodeFile receiverFile newEntries
+                  chanMsg channel $ receiver <> "'s karma got decreased to " <> show' (countKarma newEntries)
+                else do
+                  let entry = KarmaEntry
+                        { givenBy = user
+                        , givenIn = channel
+                        , givenAt = time
+                        , karmaContext = inputMessage
+                        }
+                  let oldKarma = countKarma entries
+                  triggerIncrease oldKarma channel receiver $ do
+                    oldEntries <- fromJust <$> liftIO (decodeFileStrict receiverFile)
+                    let newEntries = entry : oldEntries
+                    liftIO $ encodeFile receiverFile newEntries
   }
