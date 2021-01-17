1b3935809544692878e46f1e41599f88bf86ea68
Silvan Mosberger <contact@infinisil.com>
2021-01-17 02:30:17 +0100

    Fix fixed karma numbers

--- src/Plugins/Karma.hs
+++ src/Plugins/Karma.hs
@@ -111,8 +111,8 @@ increaseMessages =
   , (2, ((>= 0), \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to 0o" <> Text.pack (showOct (n + 1) ""))))
   , (2, ((>= 0), \n chan nick increase -> increase *> chanMsg chan (nick <> "'s karma got increased to 0x" <> Text.pack (showHex (n + 1) ""))))
   , (1, ((>= 0), \_ chan nick increase -> increase *> chanMsg chan (nick <> " was put on Santa's \"nice\" list")))
-  , (10000000, ((== 420), \_ chan nick increase -> increase *> chanMsg chan "( *∀*)y─┛   420 blaze it" *> chanMsg chan (nick <> "'s karma ^^")))
-  , (10000000, ((== 404), \_ chan nick increase -> increase *> chanMsg chan "404 Karma not found"))
+  , (10000000, ((== 419), \_ chan nick increase -> increase *> chanMsg chan "( *∀*)y─┛   420 blaze it" *> chanMsg chan (nick <> "'s karma ^^")))
+  , (10000000, ((== 403), \_ chan nick increase -> increase *> chanMsg chan "404 Karma not found"))
   ] where
   floatIncrease :: Double -> Double
   floatIncrease n = n ** logBase n (n + 1)
